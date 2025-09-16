bab416ebe0dd1e1601f842847cc04718408a54fa
querenh <81178228+querenh@users.noreply.github.com>
2025-09-16 19:48:51 -0300

    Add files via upload

---
+++ A219.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class A219 {
+    public static void main(String[] args) {
+        String salaDesejada = "A219";
+        String salaInformada = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da sala");
+        salaInformada = scanner.next();
+
+        if (salaDesejada.equals(salaInformada)) {
+            System.out.println("Você encontrou sua sala!");
+        } else {
+            System.out.println("Você está perdido!\nSe encontre!");
+        }
+        scanner.close();
+    }
+}
